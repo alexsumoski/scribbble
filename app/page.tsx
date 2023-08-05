cf67b09bed62334c6ca79db41cf08c4e672f451c
alexsumoski <alexsumoski@gmail.com>
2023-08-04 22:31:23 -0400

    Categories component, search params

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { ProjectInterface } from "@/common.types";
+import Categories from "@/components/Categories";
 import ProjectCard from "@/components/ProjectCard";
 import { fetchAllProjects } from "@/lib/actions";
 
@@ -22,14 +23,14 @@ const Home = async () => {
   if (projectsToDisplay.length === 0) {
     return (
       <section className="flexStart flex-col paddings">
-        Categories
+        <Categories />
         <p className="no-result-text text-center">No projects found.</p>
       </section>
     );
   }
   return (
     <section className="flex-start flex-col paddings mb-16">
-      categories
+      <Categories />
       <section className="projects-grid">
         {projectsToDisplay.map(({ node }: { node: ProjectInterface }) => (
           <ProjectCard
